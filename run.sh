ee899bb4dc309abdf386592465e8f74fb2b552a6
Bryan Joshua Pedini <b.pedini@bjphoster.com>
2021-09-24 09:57:19 +0200

    persistent storage

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ docker run \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/home/user/.Xauthority \
+    -v ~/.config/pac:/home/user/.config/pac \
     bryanpedini/pac
